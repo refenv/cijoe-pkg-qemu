6c1bd11178461a9161f8b85c225fffc7f4c7a984
Simon A. F. Lund <os@safl.dk>
2022-02-14 07:35:02 +0100

    bump to v6.0.4
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def read(*parts):
 
 setup(
     name="cijoe-pkg-qemu",
-    version="6.0.3",
+    version="6.0.4",
     description="CIJOE: qemu package",
     long_description=read('README.rst'),
     author="Simon A. F. Lund",
