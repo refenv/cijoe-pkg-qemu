3f361135eafaa4adfd73ebdda3e210b23ed90327
Simon A. F. Lund <os@safl.dk>
2022-03-02 10:22:10 +0100

    refenv: use default installation and user-specific guest-location
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- envs/refenv-qemu.sh
+++ envs/refenv-qemu.sh
@@ -2,19 +2,35 @@
 #
 # qemu reference environment
 #
-# In this environment the 'devbox' has qemu installed in '/opt/qemu', the installation is used to
-# instantate a qemu-guest which is used as the 'test-target'. The auxilary files for the guest are
-# stored in '/opt/guests/emujoe'
+# In this environment the 'devbox' has the qemu-binaries:
+#
+# * qemu-system-x86_64
+# * qemu-img
+#
+# Available in $PATH
+#
+# QEMU Guests are stored in $HOME/guests
+#
+# A QEMU Guest is defined with the name 'emujoe' and thus lives in $HOME/guests/emujoe
 #
 
-#
-# cijoe QEMU-module configuration
+## cijoe QEMU-module, SSH hostname and port of the machine with qemu
 export QEMU_HOST="localhost"
 export QEMU_HOST_USER="${USER}"
 export QEMU_HOST_PORT=22
-export QEMU_HOST_SYSTEM_BIN=/opt/qemu/bin/qemu-system-x86_64
+
+## cijoe QEMU-module, location of binaries on QEMU_HOST, change this to point it to a different
+# qemu-system binary which is not available in PATH eg. /opt/qemu/bin/qemu-system-x86_64
+export QEMU_HOST_SYSTEM_BIN=qemu-system-x86_64
 export QEMU_HOST_IMG_BIN=qemu-img
-export QEMU_GUESTS=/opt/guests
+
+## cijoe QEMU-module, location of guests on qemu-host
+# Files related to a qemu-guest, such as boot.img, cloud-init user-data/meta-data, pidfile, etc.
+# are stored in a subdirectory named 'QEMU_GUEST_NAME' in 'QEMU_GUESTS
+export QEMU_GUESTS=${HOME}/guests
+
+## cijoe QEMU-module, definition of a guest and how to access it via SSH
+#
 export QEMU_GUEST_NAME=emujoe
 export QEMU_GUEST_SSH_FWD_PORT=2222
 export QEMU_GUEST_CONSOLE=sock
