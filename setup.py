f758cca546903d2914795800c5995423cbdfca14
Simon A. F. Lund <os@safl.dk>
2022-10-04 10:50:16 +0200

    ver: bump to v6.1.9
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="cijoe-pkg-qemu",
-    version="6.1.8",
+    version="6.1.9",
     author="Simon A. F. Lund",
     author_email="os@safl.dk",
     url="https://github.com/refenv/cijoe-pkg-qemu/",
